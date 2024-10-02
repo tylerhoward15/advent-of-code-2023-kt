409477f9c51e7c1a197e8b8fff6538c6d8b3ca96
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2024-10-01 22:31:25 -0400

    Implement day1 part 2

--- src/Day01/Day01_part2.kt
+++ src/Day01/Day01_part2.kt
@@ -4,12 +4,51 @@ import println
 import readInput
 
 fun main() {
-    fun main(input: List<String>): Int {
-        val nums = input.map { it.filter { it.isDigit() } }
-        val ret = nums.filter { it.isNotEmpty() }.map { "${it.firstOrNull()}${it.lastOrNull()}" }.map { it.toInt() }
-            .reduceOrNull { a,b -> a + b }!!
+    val map = mutableMapOf<String, Int>(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9
+    )
 
-        return ret
+    fun getFirst(input: String): Int? {
+        var i = 0
+        while (i < input.length) {
+            if (input[i].isDigit()) return input[i].digitToInt()
+            map.keys.firstOrNull { input.substring(i).startsWith(it) }?.let { return map[it] }
+            i++
+        }
+
+        return null
+    }
+
+    fun getLast(input: String): Int? {
+        var i = input.length-1
+        while (i >= 0) {
+            if (input[i].isDigit()) return input[i].digitToInt()
+            map.keys.firstOrNull { input.substring(0, i+1).endsWith(it) }?.let { return map[it] }
+            i--
+        }
+
+        return null
+    }
+
+    fun main(input: List<String>): Int {
+        var sum = 0
+
+        for (line in input) {
+            val first = getFirst(line)
+            val second = getLast(line)
+            val num = "$first$second".toInt()
+
+            sum += num
+        }
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
@@ -17,5 +56,9 @@ fun main() {
     check(main(testInput) == 281)
 
     val input = readInput("Day01", "Day01")
+
+    val startTime = System.currentTimeMillis()
     main(input).println()
+    val endTime = System.currentTimeMillis()
+    println("Result part 2 time elapsed in milliseconds: ${endTime - startTime}")
 }
