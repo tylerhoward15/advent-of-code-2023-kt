c3feb97ef130205014517b19ef51c38838d265fe
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2024-10-01 21:28:46 -0400

    Add day 1 part 2 file

--- src/Day01/Day01_part1.kt
+++ src/Day01/Day01_part1.kt
@@ -4,7 +4,7 @@ import println
 import readInput
 
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun main(input: List<String>): Int {
         val nums = input.map { it.filter { it.isDigit() } }
         val ret = nums.filter { it.isNotEmpty() }.map { "${it.firstOrNull()}${it.lastOrNull()}" }.map { it.toInt() }
             .reduceOrNull { a,b -> a + b }!!
@@ -14,8 +14,8 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01", "Day01_part1_test")
-    check(part1(testInput) == 142)
+    check(main(testInput) == 142)
 
     val input = readInput("Day01", "Day01")
-    part1(input).println()
+    main(input).println()
 }
--- src/Day01/Day01_part2.kt
+++ src/Day01/Day01_part2.kt
@@ -1,1 +1,21 @@
 package Day01
+
+import println
+import readInput
+
+fun main() {
+    fun main(input: List<String>): Int {
+        val nums = input.map { it.filter { it.isDigit() } }
+        val ret = nums.filter { it.isNotEmpty() }.map { "${it.firstOrNull()}${it.lastOrNull()}" }.map { it.toInt() }
+            .reduceOrNull { a,b -> a + b }!!
+
+        return ret
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day01", "Day01_part2_test")
+    check(main(testInput) == 281)
+
+    val input = readInput("Day01", "Day01")
+    main(input).println()
+}
