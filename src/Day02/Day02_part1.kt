eaa8bda59bc61f785d9606878ce5248ddef79585
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2024-11-27 16:29:55 -0500

    Create skeleton for day02 part 1

--- src/Day02/Day02_part1.kt
+++ src/Day02/Day02_part1.kt
@@ -1,26 +1,39 @@
-@file:JvmName("Day02_part2Kt")
-
 package Day02
 
 import println
 import readInput
 
 fun main() {
+    fun getId(game: String): Int? {
+        val regex = "Game (\\d+):".toRegex()
+        val tmp = regex.find(game)
+        val tmps = tmp?.let{ it.groupValues[1].toInt() }
+        return tmps
+    }
+
+    fun isValid(game: String): Boolean {
+        return true
+    }
+
+    fun score(game: String): Int? {
+        if (isValid(game)) return getId(game)
+        return 0
+
+    }
+
     fun main(input: List<String>): Int {
         return input.sumOf {
-            (it.first { it.isDigit() }.digitToInt() * 10) + (it.last { it.isDigit() }.digitToInt())
+            score(it) ?: 0
         }
 
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01", "Day01_part1_test")
-    check(main(testInput) == 8)
+    val testInput = readInput("Day02", "Day02_part1_test")
+    val testOutput = main(testInput)
+    check(testOutput == 8) { "Returned $testOutput" }
 
-    val input = readInput("Day01", "Day01")
+    val input = readInput("Day02", "Day02")
 
-    val startTime = System.currentTimeMillis()
     main(input).println()
-    val endTime = System.currentTimeMillis()
-    println("Time elapsed in milliseconds: ${endTime - startTime}")
 }
