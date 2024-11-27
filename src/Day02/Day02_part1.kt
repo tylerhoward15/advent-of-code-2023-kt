d84ea0780ad6114852c954d86a37d72bc5c6f792
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2024-11-27 17:50:23 -0500

    Refactor file format

--- src/Day02/Day02_part1.kt
+++ src/Day02/Day02_part1.kt
@@ -3,40 +3,42 @@ package Day02
 import println
 import readInput
 
+fun getId(game: String): Int? {
+    val regex = "Game (\\d+):".toRegex()
+    val match = regex.find(game)
+    val id = match?.let { it.groupValues[1].toInt() }
+
+    return id
+}
+
+fun isValid(game: String): Boolean {
+    val regex = "Game (\\d+):(.*)(?:;|\$)".toRegex()
+    val match = regex.find(game)
+    println(match?.let { it.groupValues[2].trim() })
+
+    return true
+}
+
+fun score(game: String): Int? {
+    if (isValid(game)) return getId(game)
+
+    return 0
+}
+
+fun run(input: List<String>): Int {
+    return input.sumOf {
+        score(it) ?: 0
+    }
+
+}
+
 fun main() {
-    fun getId(game: String): Int? {
-        val regex = "Game (\\d+):".toRegex()
-        val match = regex.find(game)
-        val id = match?.let{ it.groupValues[1].toInt() }
-
-        return id
-    }
-
-    fun isValid(game: String): Boolean {
-        val regex = "Game (\\d+):(.*)(?:;|\$)".toRegex()
-        val match = regex.find(game)
-        println(match?.let { it.groupValues[2].trim() })
-
-        return true
-    }
-
-    fun score(game: String): Int? {
-        if (isValid(game)) return getId(game)
-    }
-
-    fun main(input: List<String>): Int {
-        return input.sumOf {
-            score(it) ?: 0
-        }
-
-    }
-
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02", "Day02_part1_test")
-    val testOutput = main(testInput)
+    val testOutput = run(testInput)
     check(testOutput == 8) { "Returned $testOutput" }
 
     val input = readInput("Day02", "Day02")
 
-    main(input).println()
+    run(input).println()
 }
