0e09571e4008de54d43a54e1891296097f1daa02
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2024-11-27 17:55:36 -0500

    Clarify main method

--- src/Day02/Day02_part1.kt
+++ src/Day02/Day02_part1.kt
@@ -39,7 +39,8 @@ fun run(input: List<String>): Int {
 
 }
 
-fun main() {
+
+fun main(args: Array<String>) {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02", "Day02_part1_test")
     val testOutput = run(testInput)
